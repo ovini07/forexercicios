aba597d5695944137bef996b4eeecfbcce68291e
Vinicius Medeiros <medeirosvini23@gmail.com>
2025-04-29 11:57:30 -0300

    ForAtividades

--- execicio3/js/script.js
+++ execicio3/js/script.js
@@ -1,19 +1,10 @@
-const numeros = [1, 2, 4, 7, 13, 16];
-
+const numeros = [1, 4, 7, 10, 13, 16];
 let quantidadePares = 0;
-let numerosPares = [];
 
-numeros.forEach(numero => {
+numeros.forEach((numero) => {
   if (numero % 2 === 0) {
     quantidadePares++;
-    numerosPares.push(numero);
   }
+
 });
-
-const resultado = document.createElement('p');
-resultado.textContent = `Quantidade de números pares: ${quantidadePares}`;
-document.body.appendChild(resultado);
-
-const listaPares = document.createElement('p');
-listaPares.textContent = `Números pares encontrados: ${numerosPares.join(', ')}`;
-document.body.appendChild(listaPares);
+alert(`Quantidade de números pares: ${quantidadePares}`);
